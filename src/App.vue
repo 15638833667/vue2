4e9764596ebef130930448d4b05c7ce00878dfb6
wangwei <wangwei@ms-battery.cn>
2023-04-07 17:45:23 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>111111</h1>
+  </div>
+  <!-- <h3>22222222</h3> -->
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  img {
+    width: 200px;
+  }
+}
+</style>
